ac9102f9ee97d370c05003e8192f7a786616c297
francocarri2506 <carrizonicolasfranco@gmail.com>
2018-10-26 01:21:42 -0300

    ventana frame

--- src/sistema/de/emergencia/medica/AsistenciaMedica.java
+++ src/sistema/de/emergencia/medica/AsistenciaMedica.java
@@ -1,6 +1,7 @@
 
 package sistema.de.emergencia.medica;
 
+
 public class AsistenciaMedica {
     
     Afiliado afiliado;
@@ -11,6 +12,40 @@ public class AsistenciaMedica {
     Enfermero enfermero;
     Doctor dotor;
     Chofer chofer;
+
+    public AsistenciaMedica(Afiliado afiliado, Movil movil, int fecha, int hora, String diagnostico, Enfermero enfermero, Doctor dotor, Chofer chofer) {
+        this.afiliado = afiliado;
+        this.movil = movil;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.diagnostico = diagnostico;
+        this.enfermero = enfermero;
+        this.dotor = dotor;
+        this.chofer = chofer;
+    }
+    
+    public void mostrarAsistenciaMedica(){
+
+    }   
+    public void verificarAfiliado(){
+
+    }   
+    public void AsignarChofer(){
+
+    }   
+    public void registrarSolicitud(){
+
+    }  
+    public void AsignarMovil(){
+
+    } 
+    public void AsignarMedico(){
+
+    } 
+    public void AsignarEnfermero(){
+
+    } 
+  
     
     
 }
