2ea56da6a3658f84d4b16b8e36bf9ff3af0dad2f
francocarri2506 <carrizonicolasfranco@gmail.com>
2018-10-26 01:21:01 -0300

    ventana frame

--- src/sistema/de/emergencia/medica/SistemaEmergenciaMedica.java
+++ src/sistema/de/emergencia/medica/SistemaEmergenciaMedica.java
@@ -7,6 +7,10 @@ public class SistemaEmergenciaMedica {
     
     public static void main(String[] args) {
         
+        
+        Ventana V1 =new Ventana();
+        
+       V1.setVisible(true);//mostrar ventana
     }
     
 }
