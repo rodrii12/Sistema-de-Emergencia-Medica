4842f692e8862ac1e9b978b222505588c2845e9d
Miguel Cejas <mbcrfull@gmail.com>
2018-11-02 14:39:33 -0300

    Borrar

--- src/sistema/de/emergencia/medica/Administrativo.java
+++ src/sistema/de/emergencia/medica/Administrativo.java
@@ -5,10 +5,20 @@ package sistema.de.emergencia.medica;
  * @author FRANCO
  */
 public class Administrativo extends Empleado{
-
+    private Integer numero;
+    
+    
     public Administrativo(Integer numeroEmpleado, String nombre, String apellido, String sexo, String dni, String direccion) {
         super(numeroEmpleado, nombre, apellido, sexo, dni, direccion);
     }
 
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+    
     
 }
